3852781ea6e80cead0e1df8930be468ce633dc7c
chinnacreations <satishthirumani1999@gmail.com>
2023-09-26 13:17:34 +0000

    Blog-Lists

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      alt="profile"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="user-name">Wade Warren</h1>
+    <p className="user-description">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
